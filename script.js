0bd5c8be315e79c346db4697069087eb29209e04
Greg Sronce <greg.sronce@gmail.com>
2021-05-26 20:14:45 -0400

    added two drop down buttons.

--- script.js
+++ script.js
@@ -34,31 +34,40 @@ const currencies = [
   'MYR'
 ]
 
-let divHeader = document.createElement('div');
-divHeader.innerHTML = '<h1>Currency Converter</h1>';
-document.getElementById('header').appendChild(divHeader);
-
-let convertFromTo = document.createElement('h2')
-convertFromTo.innerText = 'Convert from ' + 'EURO ' +  'to ' + 'currency' 
-divHeader.appendChild(convertFromTo)
-
 const url = 'http://api.exchangeratesapi.io/v1/latest?access_key=b6f09ee3c8833ec82a4cec0be6a048c9'
 
 const root = document.querySelector('#container')
 
-let currencyFrom = document.createElement('h2')
-currencyFrom.innerText = 'EUR ' + '= ' 
-root.appendChild(currencyFrom)
+let divHeader = document.createElement('div');
+divHeader.innerHTML = '<h1>Currency Converter</h1>';
+document.getElementById('header').appendChild(divHeader);
+
+let currencySelect = document.createElement('select')
+for (let currency of currencies) {
+  const optionItem = document.createElement('option')
+  optionItem.value = currency
+  optionItem.innerText = currency
+  currencySelect.appendChild(optionItem)
+  root.appendChild(currencySelect)
+}
 
 fetch(url)
 .then((res) => res.json())
 .then((data) => {
   console.log(data)
   let currencyTo = document.createElement('h3')
-  currencyTo.innerText = data.rates.AED
+  currencyTo.innerText = '1' + ' ' + data.base + '= '
   root.appendChild(currencyTo)
+  let currencySelect = document.createElement('select')
+for (let currency of currencies) {
+  const optionItem = document.createElement('option')
+  optionItem.value = currency
+  optionItem.innerText = currency
+  currencySelect.appendChild(optionItem)
+  root.appendChild(currencySelect)
+}
 })
 
-let convertButton = document.createElement('button')
-convertButton.innerText = 'Convert!'
-root.appendChild(convertButton)
+// let convertButton = document.createElement('button')
+// convertButton.innerText = 'Convert!'
+// root.appendChild(convertButton)
