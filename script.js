e2e311d2132eaf392e5f262a06d282e7609abc0f
Greg Sronce <greg.sronce@gmail.com>
2021-05-26 23:30:47 -0400

    Unorganized, need to connect everything.

--- script.js
+++ script.js
@@ -42,32 +42,62 @@ let divHeader = document.createElement('div');
 divHeader.innerHTML = '<h1>Currency Converter</h1>';
 document.getElementById('header').appendChild(divHeader);
 
-let currencySelect = document.createElement('select')
+let currencyFrom = document.createElement('select')
+currencyFrom.classList.add('country-from')
 for (let currency of currencies) {
   const optionItem = document.createElement('option')
   optionItem.value = currency
   optionItem.innerText = currency
-  currencySelect.appendChild(optionItem)
-  root.appendChild(currencySelect)
+  currencyFrom.appendChild(optionItem)
+  root.appendChild(currencyFrom)
+}
+
+let currencyTo = document.createElement('select')
+currencyTo.classList.add('country-to')
+for (let currency of currencies) {
+  const optionItem = document.createElement('option')
+  optionItem.value = currency
+  optionItem.innerText = currency
+  currencyTo.appendChild(optionItem)
+  root.appendChild(currencyTo)
 }
 
 fetch(url)
 .then((res) => res.json())
 .then((data) => {
   console.log(data)
-  let currencyTo = document.createElement('h3')
-  currencyTo.innerText = '1' + ' ' + data.base + '= '
-  root.appendChild(currencyTo)
-  let currencySelect = document.createElement('select')
-for (let currency of currencies) {
-  const optionItem = document.createElement('option')
-  optionItem.value = currency
-  optionItem.innerText = currency
-  currencySelect.appendChild(optionItem)
-  root.appendChild(currencySelect)
-}
-})
+  // let currencyTo = document.createElement('h4')
+  // currencyTo.innerText = '1' + ' ' + data.base + '= '
+  // divFooter.appendChild(currencyTo)
+})   
 
-// let convertButton = document.createElement('button')
-// convertButton.innerText = 'Convert!'
-// root.appendChild(convertButton)
+
+let divFooter = document.createElement('div')
+document.getElementById('footer').appendChild(divFooter);
+
+let input = document.createElement('input')
+input.classList.add('number-button')
+divFooter.appendChild(input)
+
+let convertButton = document.createElement('button')
+convertButton.classList.add('convert-button')
+convertButton.innerText = 'Convert!'
+divFooter.appendChild(convertButton)
+
+let output = document.createElement('h3')
+output.classList.add('output')
+divFooter.appendChild(output)
+
+document.querySelector('.convert-button').addEventListener('click', function (event) {
+  console.log(event.target)})
+
+// addEventListener('click', function (event) {
+//   console.log(event.target.innerText)
+//   output.innerText += event.target.innerText})
+
+// convertButton.addEventListener('click', function (event) {
+//   const display = eval(output.innerText)
+//   output.innerText = display
+//   console.log(eval(output.innerText))})
+  
+// if or statement?
